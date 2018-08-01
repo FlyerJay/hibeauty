92ea15b8e78b2501bea955401787ece2e6589124
flyerjay <876173839@qq.com>
2018-08-01 11:29:18 +0800

    修改

--- app/web/component/layout/app/main.vue
+++ app/web/component/layout/app/main.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="page-container">
+  <div class="page-container" ref="container">
     <slot name="main"></slot>
   </div>
 </template>
 <style lang="less">
   
 </style>
-<script type="text/babel">
+<script>
+  export default{
+    mounted() {
+      this.$refs.container.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+        console.log(1);
+      }, { passive: false });
+    }
+  };
 </script>
