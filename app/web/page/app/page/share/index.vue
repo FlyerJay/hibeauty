866c8eb84502d8b6d3a493d25d676cbf843f9e7d
flyerjay <876173839@qq.com>
2018-08-29 21:57:01 +0800

    你的好友送你一张优惠券

--- app/web/page/app/page/share/index.vue
+++ app/web/page/app/page/share/index.vue
@@ -4,10 +4,10 @@
 <script>
   import axios from 'axios';
   const wxConfig = {
-    title: '戒易花', // 分享标题
-    desc: '送你一张优惠券', // 分享描述
-    link: 'http://i.snowyet.cc', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-    imgUrl: '', // 分享图标
+    title: '你的好友送你一张优惠券', // 分享标题
+    desc: '你的好友送你一张优惠券', // 分享描述
+    link: 'https://mxj.zbj.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+    imgUrl: 'https://p6.zbjimg.com//task/2018-05/09/theme/5af263f1c069b.jpg', // 分享图标
     type: 'link', // 分享类型,music、video或link，不填默认为link
     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
   };
