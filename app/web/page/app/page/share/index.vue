b35fa438da7835deafa5db8d47b046907917ed99
flyerjay <876173839@qq.com>
2018-09-21 18:51:50 +0800

    ceshi

--- app/web/page/app/page/share/index.vue
+++ app/web/page/app/page/share/index.vue
@@ -6,7 +6,7 @@
   const wxConfig = {
     title: '我的测试文案', // 分享标题
     desc: '我的测试文案', // 分享描述
-    link: 'http://i.snowyet.cc', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+    link: 'http://i.snowyet.cc/asdsd/ka', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
     imgUrl: 'https://p6.zbjimg.com//task/2018-05/09/theme/5af263f1c069b.jpg', // 分享图标
     type: 'link', // 分享类型,music、video或link，不填默认为link
     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
