b00bb1cbf9731f17d0184be4594d4b7a60340d15
flyerjay <876173839@qq.com>
2018-10-17 09:23:51 +0800

    update: 添加地图控件

--- app/web/page/app/page/bmap/index.vue
+++ app/web/page/app/page/bmap/index.vue
@@ -6,19 +6,41 @@
 export default {
   data() {
     return {
-      codinrate: {}, // 定位坐标
-      mapInstance: {}, // 地图实例
+      codinrate: { x: 106.497138, y: 29.627971 }, // 定位坐标
+      map: {}, // 地图实例
+      point: {}, // 地图坐标点
     };
   },
 
   mounted() {
-    this.mapInstance = new BMap.Map('bmap');
-    let point = new BMap.Point(116.404, 39.915);
-    // 创建点坐标
-    this.mapInstance.centerAndZoom(point, 15);
+    // 初始化百度地图
+    this.initBmap();
+    // 初始化地图控件
+    this.initControl();
+    // 标注所在位置
+    this.markerPosition();
   },
 
   methods: {
+    initBmap() {
+      this.map = new BMap.Map('bmap');
+      this.point = new BMap.Point(this.codinrate.x, this.codinrate.y);
+      // 创建点坐标
+      this.map.centerAndZoom(this.point, 15);
+    },
+
+    initControl() {
+      this.map.addControl(new BMap.NavigationControl());
+      this.map.addControl(new BMap.ScaleControl());
+      this.map.addControl(new BMap.OverviewMapControl());
+      this.map.addControl(new BMap.MapTypeControl());
+      this.map.setCurrentCity('重庆');
+    },
+
+    markerPosition() {
+      const maker = new BMap.Marker(this.point);
+      this.map.addOverlay(maker);
+    }
   }
 };
 </script>
